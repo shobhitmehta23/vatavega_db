3e6f04c3c8438307abdecad619909abd7ad60489
Shobhit Mehta <shobhit@Shobhits-MacBook-Pro.local>
2018-02-17 18:28:00 -0500

    added files for 2nd assignment

---
+++ src/a2test/DBFile.h
@@ -0,0 +1,40 @@
+#ifndef DBFILE_H
+#define DBFILE_H
+
+#include "TwoWayList.h"
+#include "Record.h"
+#include "Schema.h"
+#include "File.h"
+#include "Comparison.h"
+#include "ComparisonEngine.h"
+#include "File.h"
+#include "cstdlib"
+
+typedef enum {heap, sorted, tree} fType;
+
+
+class DBFile {
+private:
+	File file;
+	Page page;
+	off_t current_page_index;
+
+	// utility methods to help with indexes
+	int getLastPageIndex();
+	int getNewPageIndex();
+
+public:
+	DBFile (); 
+
+	int Create (const char *fpath, fType file_type, void *startup);
+	int Open (const char *fpath);
+	int Close ();
+
+	void Load (Schema &myschema, const char *loadpath);
+
+	void MoveFirst ();
+	void Add (Record &addme);
+	int GetNext (Record &fetchme);
+	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
+};
+#endif
