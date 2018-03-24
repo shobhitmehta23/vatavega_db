173cf16b61023ff0232eed12913afee9ee7547a8
digvijay1988 <dkulkarni@ufl.edu>
2018-03-23 21:05:26 -0400

    implemented 2 operators

--- src/a3test/RelOp.cc
+++ src/a3test/RelOp.cc
@@ -1,12 +1,102 @@
 #include "RelOp.h"
 
-void SelectFile::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+void *selectFile(void *thread_args);
+void *selectPipe(void *thread_args);
+
+/*
+ * ---------------------------SelectFile----------------------------------------
+ */
+
+void SelectFile::Run(DBFile &inFile, Pipe &outPipe, CNF &selOp,
+		Record &literal) {
+
+	//create a struc object to hold all the arguments to be passed to the thread.
+	relOp_thread_arguments *args = new relOp_thread_arguments;
+	args->inFile = &inFile;
+	args->outPipe = &outPipe;
+	args->selOp = &selOp;
+	args->literal = &literal;
+
+	//spawn the thread and pass the arguments.
+	pthread_create(&thread, NULL, selectPipe, (void *) args);
+
+	//Shut down output pipe in the end.
+	outPipe.ShutDown();
+
+	delete args;
+
 }
 
-void SelectFile::WaitUntilDone () {
-	// pthread_join (thread, NULL);
+void SelectFile::WaitUntilDone() {
+	pthread_join(thread, NULL);
 }
 
-void SelectFile::Use_n_Pages (int runlen) {
+void SelectFile::Use_n_Pages(int runlen) {
+	this->runlen = runlen;
+}
 
+void *selectFile(void *thread_args) {
+	relOp_thread_arguments *args;
+	args = (relOp_thread_arguments *) thread_args;
+	DBFile *inFile = args->inFile;
+	Pipe *outPipe = args->outPipe;
+	Record *literal = args->literal;
+	CNF *selOp = args->selOp;
+
+	Record* temp_rec = new Record;
+	while (inFile->GetNext(*temp_rec, *selOp, *literal)) {
+		outPipe->Insert(temp_rec);
+	}
+	delete temp_rec;
+}
+/*
+ * ---------------------------SelectPipe----------------------------------------
+ */
+
+void SelectPipe::Run(Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) {
+	//create a struc object to hold all the arguments to be passed to the thread.
+	relOp_thread_arguments *args = new relOp_thread_arguments;
+	args->inPipe = &inPipe;
+	args->outPipe = &outPipe;
+	args->selOp = &selOp;
+	args->literal = &literal;
+
+	//spawn the thread and pass the arguments.
+	pthread_create(&thread, NULL, selectPipe, (void *) args);
+
+	//Shut down output pipe in the end.
+	outPipe.ShutDown();
+
+	delete args;
+}
+
+/*
+ * Implementation of the 'Select pipe' operation.
+ * This function will be executed by the thead in the Run method.
+ */
+void *selectPipe(void *thread_args) {
+	relOp_thread_arguments *args;
+	args = (relOp_thread_arguments *) thread_args;
+	Pipe *inPipe = args->inPipe;
+	Pipe *outPipe = args->outPipe;
+	Record *literal = args->literal;
+	CNF *selOp = args->selOp;
+
+	ComparisonEngine compEngine;
+
+	Record* temp_rec = new Record;
+	while (inPipe->Remove(temp_rec)) {
+		if (compEngine.Compare(temp_rec, literal, selOp)) {
+			outPipe->Insert(temp_rec);
+		}
+	}
+	delete temp_rec;
+}
+
+void SelectPipe::WaitUntilDone() {
+	pthread_join(thread, NULL);
+}
+
+void SelectPipe::Use_n_Pages(int runlen) {
+	this->runlen = runlen;
 }
--- src/a3test/RelOp.h
+++ src/a3test/RelOp.h
@@ -7,69 +7,102 @@
 #include "Function.h"
 
 class RelationalOp {
-	public:
+public:
 	// blocks the caller until the particular relational operator 
 	// has run to completion
-	virtual void WaitUntilDone () = 0;
+	virtual void WaitUntilDone() = 0;
 
 	// tell us how much internal memory the operation can use
-	virtual void Use_n_Pages (int n) = 0;
+	virtual void Use_n_Pages(int n) = 0;
 };
 
-class SelectFile : public RelationalOp { 
+typedef struct relOp_thread_arguments {
+	Pipe *inPipe;
+	Pipe *outPipe;
+	CNF *selOp;
+	Record *literal;
+	DBFile *inFile;
+} relOp_thread_arguments;
 
-	private:
-	// pthread_t thread;
+class SelectFile: public RelationalOp {
+
+private:
+	pthread_t thread;
+	int runlen = 1;
 	// Record *buffer;
 
-	public:
+public:
 
-	void Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal);
-	void WaitUntilDone ();
-	void Use_n_Pages (int n);
+	void Run(DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal);
+	void WaitUntilDone();
+	void Use_n_Pages(int n);
 
 };
 
-class SelectPipe : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class SelectPipe: public RelationalOp {
+private:
+	pthread_t thread;
+	int runlen = 1;
+public:
+	void Run(Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal);
+	void WaitUntilDone();
+	void Use_n_Pages(int n);
 };
-class Project : public RelationalOp { 
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class Project: public RelationalOp {
+public:
+	void Run(Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput,
+			int numAttsOutput) {
+	}
+	void WaitUntilDone() {
+	}
+	void Use_n_Pages(int n) {
+	}
 };
-class Join : public RelationalOp { 
-	public:
-	void Run (Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class Join: public RelationalOp {
+public:
+	void Run(Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp,
+			Record &literal) {
+	}
+	void WaitUntilDone() {
+	}
+	void Use_n_Pages(int n) {
+	}
 };
-class DuplicateRemoval : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class DuplicateRemoval: public RelationalOp {
+public:
+	void Run(Pipe &inPipe, Pipe &outPipe, Schema &mySchema) {
+	}
+	void WaitUntilDone() {
+	}
+	void Use_n_Pages(int n) {
+	}
 };
-class Sum : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class Sum: public RelationalOp {
+public:
+	void Run(Pipe &inPipe, Pipe &outPipe, Function &computeMe) {
+	}
+	void WaitUntilDone() {
+	}
+	void Use_n_Pages(int n) {
+	}
 };
-class GroupBy : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class GroupBy: public RelationalOp {
+public:
+	void Run(Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts,
+			Function &computeMe) {
+	}
+	void WaitUntilDone() {
+	}
+	void Use_n_Pages(int n) {
+	}
 };
-class WriteOut : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, FILE *outFile, Schema &mySchema) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class WriteOut: public RelationalOp {
+public:
+	void Run(Pipe &inPipe, FILE *outFile, Schema &mySchema) {
+	}
+	void WaitUntilDone() {
+	}
+	void Use_n_Pages(int n) {
+	}
 };
 #endif
