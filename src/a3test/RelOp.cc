3a0b2df8ff5ec335c246ebb71c94315d741c6796
Shobhit Mehta <shobhit@Shobhits-MacBook-Pro.local>
2018-03-26 21:58:50 -0400

    better handling of random file names to avoid conflicting file names

--- src/a3test/RelOp.cc
+++ src/a3test/RelOp.cc
@@ -2,6 +2,7 @@
 #include <sstream>
 #include "BigQ.h"
 #include <string.h>
+#include <atomic>
 
 //Method declarations:
 void *selectFile(void *thread_args);
@@ -609,7 +610,8 @@ void blockMergeJoin(Pipe *inPipeL, Pipe *inPipeR, Pipe *outPipe, CNF *selOp,
 
 	DBFile temp_dbfile;
 	char * temp_dbfile_name = (char*) malloc(20 * sizeof(char));
-	sprintf(temp_dbfile_name, "temp_db_%d", rand() % 1000);
+	static std::atomic_int count;
+	sprintf(temp_dbfile_name, "temp_db_%d%d", rand() % 1000, count++);
 	temp_dbfile.Create(temp_dbfile_name, heap, NULL);
 
 	Record *temp_record = new Record;
