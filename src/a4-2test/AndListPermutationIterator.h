36ade17f0ac244d43b59bab44ae0eeb01a29c8ba
Shobhit Mehta <shobhit@Shobhits-MacBook-Pro.local>
2018-04-22 19:10:16 -0400

    andlist permutation complete

--- src/a4-2test/AndListPermutationIterator.h
+++ src/a4-2test/AndListPermutationIterator.h
@@ -1,12 +1,30 @@
 #ifndef A4_2TEST_ANDLISTPERMUTATIONITERATOR_H_
 #define A4_2TEST_ANDLISTPERMUTATIONITERATOR_H_
 
-class  AndListPermutationIterator {
+#include "ParseTree.h"
+
+#include <vector>
+
+using namespace std;
+
+class AndListPermutationIterator {
+private:
+	struct AndList *andList;
+	int countOfNodes;
+	long numberOfPermutations;
+	vector<vector<int>> *permutations;
+	struct AndList **arrayOfAnds;
+	int permutationCounter = 0;
+
+	vector<vector<int>> * getPermutations(int *arr, int size);
+	int getCountOfNodes();
+	long getNumberOfPermutations(int n);
+	struct AndList * copyAndListNode(struct AndList *);
 public:
 	AndListPermutationIterator(struct AndList *andList);
+	~AndListPermutationIterator();
 	struct AndList * getNext();
+	void destroyPermutation(struct AndList * andList);
 };
-
-
 
 #endif /* A4_2TEST_ANDLISTPERMUTATIONITERATOR_H_ */
