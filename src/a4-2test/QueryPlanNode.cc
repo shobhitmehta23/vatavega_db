939b1fa40bfb5e133477001cbce2aa7e5cdda5d7
digvijay1988 <dkulkarni@ufl.edu>
2018-04-23 00:09:19 -0400

    partial code to avoid conflicts

--- src/a4-2test/QueryPlanNode.cc
+++ src/a4-2test/QueryPlanNode.cc
@@ -27,6 +27,35 @@ void SelectFileNode::printNode() {
 
 }
 
+void SelectFileNode::applySelectCondition(AndList* andList, Statistics &stats) {
+	AndList* ands = andList;
+
+	AndList* selectAndList = new AndList();
+	AndList* currentAndListPtr = selectAndList;
+	while (ands != NULL) {
+		bool goToNextAnd = false;
+		OrList ors = ands->left;
+		while (ors != NULL) {
+			if (ors.left != NULL) {
+				Operand *left = ors.left->left;
+				Operand *right = ors.left->right;
+
+				if (left->code == NAME && right->code == NAME) {
+					goToNextAnd = true;
+					break;
+				}
+
+			}
+			ors = ors.rightOr;
+		}
+		if (goToNextAnd) {
+			continue;
+		}
+		currentAndListPtr->left = ands->left;
+		ands = ands->rightAnd;
+	}
+}
+
 void SelectPipeNode::printNode() {
 
 }
